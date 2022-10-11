fafa7554fd89d3e8dcc404f656fd219d0cfeaed7
thientrile <thientrile2003@gmail.com>
2022-10-11 20:32:33 +0700

    update

--- wp-content/plugins/wptd-video-popup/assets/js/wptd-video-popup.js
+++ wp-content/plugins/wptd-video-popup/assets/js/wptd-video-popup.js
@@ -36,6 +36,24 @@
 			removalDelay: 160,
 			preloader: false,
 			fixedContentPos: false,
+			iframe: {
+				markup: '<div class="mfp-iframe-scaler">' +
+					'<div class="mfp-close"></div>' +
+					'<iframe class="mfp-iframe" frameborder="0" allow="autoplay" allowfullscreen></iframe>' +
+					'</div>',
+				patterns: {
+					youtube: {
+						index: 'youtube.com/',
+						id: 'v=',
+						src: 'https://www.youtube.com/embed/%id%?autoplay=0'
+					},
+					vimeo: {
+						index: 'vimeo.com/',
+						id: '/',
+						src: '//player.vimeo.com/video/%id%?autoplay=1&loop=1&autopause=0'
+					},
+				}
+			},
 			callbacks: {
 				open: function() {
 					$("body").addClass(_t_class);
@@ -95,7 +113,7 @@
 			iframe: {
 				markup: '<div class="mfp-iframe-scaler">' +
 					'<div class="mfp-close"></div>' +
-					'<iframe class="mfp-iframe" frameborder="0" allow="autoplay"></iframe>' +
+					'<iframe class="mfp-iframe" frameborder="0" allow="autoplay" allowfullscreen></iframe>' +
 					'</div>',
 				patterns: {
 					youtube: {
